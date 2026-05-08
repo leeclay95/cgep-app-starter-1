0a6555b180ea76a2a0cdc495c72f4dd1ff4ef920
Lee Clayton <leehart32@gmail.com>
2026-05-07 22:18:04 -0600

    test: intentional AC.L2-3.1.5 violation

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -16,11 +16,3 @@ output "uploads_bucket" {
 output "lambda_function_name" {
   value = aws_lambda_function.intake.function_name
 }
-
-output "vpc_id" {
-  value = aws_vpc.main.id
-}
-
-output "private_subnet_ids" {
-  value = aws_subnet.private[*].id
-}
